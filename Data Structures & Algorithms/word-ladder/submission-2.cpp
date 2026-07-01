36503637a53a2c37aa664f9aad29947fa1df5c17
Giulio Bronson <77513808+giuliobronson@users.noreply.github.com>
2026-07-01 11:50:11 -0300

    Add: word-ladder - submission-2

---
+++ Data Structures & Algorithms/word-ladder/submission-2.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<string> neighbors(string& s) {
+        vector<string> res;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s[i];
+            for (int j = 0; j < 26; j++) {
+                if (c == 'a' + j) continue;
+                s[i] = 'a' + j;
+                res.push_back(s);
+            }
+            s[i] = c;
+        }
+
+        return res;
+    }
+
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> words(wordList.begin(), wordList.end());
+        unordered_set<string> visited;
+        queue<pair<string, int>> q;
+
+        q.push({beginWord, 1});
+        while (!q.empty()) {
+            auto [u, d] = q.front(); q.pop();
+
+            if (u == endWord) return d;
+
+            if (visited.count(u)) continue;
+
+            visited.insert(u);
+
+            for (string& v: neighbors(u)) {
+                if (words.count(v)) q.push({v, d + 1});
+            }
+        }
+
+        return 0;
+    }
+};
