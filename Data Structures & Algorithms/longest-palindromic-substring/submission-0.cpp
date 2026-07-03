a25def0c9585d11341531d1f1af66e3106ec06ab
Giulio Bronson <77513808+giuliobronson@users.noreply.github.com>
2026-07-03 13:51:26 -0300

    Add: longest-palindromic-substring - submission-0

---
+++ Data Structures & Algorithms/longest-palindromic-substring/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int l = 0, r = s.length() - 1;
+
+        while (l < r) {
+            if (s[l++] != s[r--]) return false;
+        }
+
+        return true;
+    }
+
+    string longestPalindrome(string s) {
+        int maxL = 0, start = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + maxL; j < s.length(); j++) {
+                if (isPalindrome(s.substr(i, j - i + 1))) {
+                    maxL = max(maxL, j - i + 1);
+                    start = i;
+                }
+            }
+        }
+
+        return s.substr(start, maxL);
+    }
+};
