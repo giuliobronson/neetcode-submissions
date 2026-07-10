f4c069bf41d6621423bcee8aac8289a3e8d81833
Giulio Bronson <77513808+giuliobronson@users.noreply.github.com>
2026-07-10 10:37:09 -0300

    Add: design-twitter-feed - submission-4

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-4.cpp
@@ -0,0 +1,56 @@
+class Twitter {
+public:
+    struct Post {
+        int t;
+        int tweetId;
+    };
+
+    unordered_map<int, unordered_set<int>> followees;
+    unordered_map<int, vector<Post>> posts;
+    int time = 0;
+
+    Twitter() {
+        
+    }
+    
+    void postTweet(int userId, int tweetId) {
+        posts[userId].push_back({time++, tweetId});
+    }
+    
+    vector<int> getNewsFeed(int userId) {
+        priority_queue<Post, vector<Post>, decltype([](Post& p1, Post& p2) {
+            return p1.t < p2.t;
+        })> pq(posts[userId].rbegin(), posts[userId].rbegin() + min((int) posts[userId].size(), 10));
+        vector<int> feed;
+        
+        cout << pq.size() << '\n';
+
+        for (int followee : followees[userId]) {
+            for (auto it = posts[followee].rbegin(); it != posts[followee].rbegin() + min((int) posts[followee].size(), 10); it++) {
+                // cout << it->t << " " << it-> tweetId << '\n';
+                pq.push(*it);
+            }
+        }
+        
+        cout << pq.size() << '\n';
+
+        while (!pq.empty()) {
+            Post p = pq.top(); pq.pop();
+
+            feed.push_back(p.tweetId);
+
+            if (feed.size() == 10) return feed;
+        }
+
+        return feed;
+    }
+    
+    void follow(int followerId, int followeeId) {
+        if (followerId == followeeId) return;
+        followees[followerId].insert(followeeId);
+    }
+    
+    void unfollow(int followerId, int followeeId) {
+        followees[followerId].erase(followeeId);
+    }
+};
